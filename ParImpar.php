31be0baae82501fa0a566d78a7fab9752557887b
TioMarco <marcomariobros68@gmail.com>
2021-10-21 12:37:08 -0300

    Update

---
+++ ParImpar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+</head>
+<style type="text/css">
+body {
+margin:0;
+padding:0;
+background:#cccccc;
+text-align:center; 
+	}
+#tudo {
+width: 760px;
+margin:0 auto;			
+padding: 10px;
+text-align:left; 
+	}
+#conteudo {
+padding: 5px;
+background-color: #eeeeee;
+	}
+</style>
+</head>
+<body>
+<?php include "menu.php"?> 
+    
+</body>
+</html>
--- menu.php
+++ menu.php
@@ -1,3 +1,4 @@
+<a href="ParImpar.php">ParImpar</a>
 <a href="jokenpo.php">Jokenpo</a>
 <a href="dados.php">Dados</a>
 <br><br>
